6abd72bfbdd44777554e180bd992f395c3340e9c
gsierra22 <giancarlosierra2011@hotmail.com>
2022-11-13 21:17:31 -0600

    palindrome

--- reverse.js
+++ reverse.js
@@ -1,11 +1,11 @@
-// const revString = (str)=>{
-//     const arr = str.split("");
-//     arr.reverse();
-//     str = arr.join("");
-//     console.log(str);
-// }
+const revString = (str)=>{
+    const arr = str.split("");
+    arr.reverse();
+    str = arr.join("");
+    console.log(str);
+}
 
-// revString('hello');
+revString('hello');
 
 (function(str){
     let reversed = ''
