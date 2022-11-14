948f69a03691b650de263d51a8bfe9c6764acbe5
gsierra22 <giancarlosierra2011@hotmail.com>
2022-11-13 21:13:17 -0600

    added second function

--- reverse.js
+++ reverse.js
@@ -1,8 +1,18 @@
-const revString = (str)=>{
-    const arr = str.split("");
-    arr.reverse();
-    str = arr.join("");
-    console.log(str);
-}
+// const revString = (str)=>{
+//     const arr = str.split("");
+//     arr.reverse();
+//     str = arr.join("");
+//     console.log(str);
+// }
 
-revString('hello')
+// revString('hello');
+
+(function(str){
+    let reversed = ''
+    for(let character of str){
+        reversed=character + reversed
+    }
+    console .log(
+        reversed
+    )
+})('saladguy');
