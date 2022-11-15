c7d2edd1ac10d67f0c4f8436e13ad1318c0b18e7
gsierra22 <giancarlosierra2011@hotmail.com>
2022-11-14 23:30:20 -0600

    added fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,15 @@
+let fizzbuzz = (num) =>{
+    if (num % 3 === 0 && num % 5 === 0){
+        console.log('fizzbuzz')
+    }
+    else if (num % 5 === 0){
+        console.log('buzz')
+    }
+    else if (num % 3 === 0){
+        console.log('fizz')
+    }
+    else{console.log("no output")}
+    return
+}
+
+fizzbuzz(8)
