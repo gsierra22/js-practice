75c2cbdceba44f3e85ef6befc07acc4634aa08d1
gsierra22 <giancarlosierra2011@hotmail.com>
2022-11-14 23:56:32 -0600

    second vowel count added

--- vowelCount.js
+++ vowelCount.js
@@ -1,14 +1,30 @@
-const vowels = ['a', 'e', 'i', 'o', 'u'] 
+const vowels = ['a', 'e', 'i', 'o', 'u'];
 
 let vowelCount = (str) => {
-    let count =0
+    let count =0;
     for(let char of str.toLowerCase()){
         if (vowels.includes(char)){
             count++
         }
     }
-    console.log(count)
-}
+    console.log(count);
+    return count;
+};
 
-vowelCount('apple')
-vowelCount('daedra')
+let vowelCount2 =(str)=>{
+    let count =0;
+    for(let i=0; i<str.length;i++){
+        for(let j=0; j<vowels.length;j++){
+            if (str[i]===vowels[j]){
+                count++
+            }
+        }
+    }
+    console.log(count);
+    return count;
+};
+
+vowelCount('apple');
+vowelCount('daedra');
+vowelCount2('queue');
+vowelCount2('thunder');
