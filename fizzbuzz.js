37e08784d3535efd5d66ac7d1dec3f4515089966
gsierra22 <giancarlosierra2011@hotmail.com>
2022-11-17 22:11:11 -0600

    ternary fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -12,4 +12,15 @@ let fizzbuzz = (num) =>{
     return
 }
 
+const fizzBuzz2 = (num)=>{
+    num % 3 === 0 && num % 5 === 0  ? console.log('fizzbuzz') : 
+    num % 5 === 0 ? console.log('buzz') :
+    num % 3 === 0 ? console.log('fizz') : console.log("no output")
+    return
+}
+
 fizzbuzz(8)
+fizzBuzz2(9)
+fizzBuzz2(10)
+fizzBuzz2(15)
+fizzBuzz2(8)
